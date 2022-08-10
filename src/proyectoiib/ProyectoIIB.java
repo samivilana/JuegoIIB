ac6797424bb064019e36c06c5662408e2a4c84d2
PC-DIANA <PC-DIANA@DESKTOP-P11TRHB>
2022-08-09 19:41:17 -0500

    Caro casi termino mi parte xd, loviu <3

--- src/proyectoiib/ProyectoIIB.java
+++ src/proyectoiib/ProyectoIIB.java
@@ -17,6 +17,7 @@ public class ProyectoIIB {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola ");
+        System.out.println("Que mas xd");
     }
     
 }
